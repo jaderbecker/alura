89e1e93c5b27d60f9b3f1f72dad7bf4d2867a87e
Jader Becker <148784317+jaderbecker@users.noreply.github.com>
2024-02-25 01:49:20 -0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
   res.writeHead(302, {
-    'Location': 'https://api.whatsapp.com/send?phone=SEU_NUMERO_DE_TELEFONE'
+    'Location': 'https://wa.me/5551998082464?text=Ol%C3%A1%21+Tenho+interesse+e+queria+mais+informa%C3%A7%C3%B5es%2C+por+favor.'
   });
   res.end();
 };
