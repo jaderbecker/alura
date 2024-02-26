75e84645b83a45dce7f00d0ae348d9e56bf22bfa
Jader Becker <148784317+jaderbecker@users.noreply.github.com>
2024-02-26 10:08:57 -0300

    Delete redirect.js

--- redirect.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.writeHead(302, {
-    'Location': 'https://wa.me/5551998082464?text=Ol%C3%A1%21+Tenho+interesse+e+queria+mais+informa%C3%A7%C3%B5es%2C+por+favor.'
-  });
-  res.end();
-};
